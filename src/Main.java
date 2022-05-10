17d8536b85f07521275b035c01641652cc6aa732
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-10 15:05:32 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args)  {
     Product productGas = new Product(4.0, 10.0, "gas");
     productGas.totalCost(4);
-    productGas.printProduct("gas", 4, 10);  
+    productGas.printProduct("gas", 4, 10); 
+    System.out.println(productGas);
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -5,6 +5,9 @@ public class Product {
 
     //constructor method
     public Product(double productCost, double quantityofProduct, String productName){
+        this.productCost = productCost;
+        this.quantityofProduct = quantityofProduct;
+        this.productName = productName;
     }
 
     //other methods
@@ -15,5 +18,9 @@ public class Product {
     //prints how much of one unit cost and how many times purchased
     public void printProduct(String productName, double productCost, double quantityofProduct ){
         System.out.println(productName + " costs " + productCost + " and " + quantityofProduct + " units were purchased");
+    }
+
+    public String toString(){
+        return productName + " costs " + productCost + " and " + quantityofProduct + " units were purchased";
     }
 }
